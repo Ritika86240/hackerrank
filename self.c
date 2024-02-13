42a393d3b77b56e2fc5b1b35068bb685d05f7d63
Ritika86240 <146440022+Ritika86240@users.noreply.github.com>
2024-02-13 23:09:37 +0530

    Add files via upload

---
+++ self.c
@@ -0,0 +1,15 @@
+/*#include<stdio.h>
+void main ()
+{
+    int a=10,b ;
+   b=a+++a;
+   printf("%d",b);
+}*/
+
+#include<stdio.h>
+void  main()
+{
+    int a=10,b=11,c;c=a+++++b+b--+b++;
+    pritnf("%d\n",c);
+
+         output 45}
