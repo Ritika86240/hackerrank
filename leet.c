7de92382992926bd6e4ed03d3e5f995f085da1f2
Ritika86240 <146440022+Ritika86240@users.noreply.github.com>
2024-02-13 22:53:46 +0530

    Add files via upload

---
+++ leet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ int main()
+{
+
+int array[],i,m,n,j,dn; 
+printf("enter the size of the array");
+scanf("%d%d",&m,&n);
+
+ for(i=0;i<m;i++)
+{
+  for(j=0;j<n;j++)
+  array[i][j]=dn[i+j];
+
+}
+printf("array elemnets[%d]",dn (i+j));
+printf("\n");
+return 0;}
